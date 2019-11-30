5cbe4f2d9a595df32cbeeb4c6bfa223a1c9be50e
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-11-30 15:40:34 +0700

    Initial commit

---
+++ app/src/main/java/com/rizkykhapidsyah/p_scrollview__jvm/MainActivity.java
@@ -0,0 +1,50 @@
+package com.rizkykhapidsyah.p_scrollview__jvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView TV_Demo1_IDJAVA, TV_Demo2_IDJAVA;
+    Button B_Button1_IDJAVA, B_Button2_IDJAVA, B_Button3_IDJAVA, B_Button4_IDJAVA, B_Button5_IDJAVA,
+            B_Button6_IDJAVA, B_Button7_IDJAVA, B_Button8_IDJAVA, B_Button9_IDJAVA, B_Button10_IDJAVA,
+            B_Button11_IDJAVA, B_Button12_IDJAVA, B_Button13_IDJAVA, B_Button14_IDJAVA, B_Button15_IDJAVA,
+            B_Button16_IDJAVA, B_Button17_IDJAVA, B_Button18_IDJAVA, B_Button19_IDJAVA, B_Button20_IDJAVA,
+            B_Button21_IDJAVA, B_Button22_IDJAVA, B_Button23_IDJAVA, B_Button24_IDJAVA, B_Button25_IDJAVA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TV_Demo1_IDJAVA = findViewById(R.id.TV_Demo1_IDXML);
+        TV_Demo2_IDJAVA = findViewById(R.id.TV_Demo2_IDXML);
+        B_Button1_IDJAVA = findViewById(R.id.B_Button1_IDXML);
+        B_Button2_IDJAVA = findViewById(R.id.B_Button2_IDXML);
+        B_Button3_IDJAVA = findViewById(R.id.B_Button3_IDXML);
+        B_Button4_IDJAVA = findViewById(R.id.B_Button4_IDXML);
+        B_Button5_IDJAVA = findViewById(R.id.B_Button5_IDXML);
+        B_Button6_IDJAVA = findViewById(R.id.B_Button6_IDXML);
+        B_Button7_IDJAVA = findViewById(R.id.B_Button7_IDXML);
+        B_Button8_IDJAVA = findViewById(R.id.B_Button8_IDXML);
+        B_Button9_IDJAVA = findViewById(R.id.B_Button9_IDXML);
+        B_Button10_IDJAVA = findViewById(R.id.B_Button10_IDXML);
+        B_Button11_IDJAVA = findViewById(R.id.B_Button11_IDXML);
+        B_Button12_IDJAVA = findViewById(R.id.B_Button12_IDXML);
+        B_Button13_IDJAVA = findViewById(R.id.B_Button13_IDXML);
+        B_Button14_IDJAVA = findViewById(R.id.B_Button14_IDXML);
+        B_Button15_IDJAVA = findViewById(R.id.B_Button15_IDXML);
+        B_Button16_IDJAVA = findViewById(R.id.B_Button16_IDXML);
+        B_Button17_IDJAVA = findViewById(R.id.B_Button17_IDXML);
+        B_Button18_IDJAVA = findViewById(R.id.B_Button18_IDXML);
+        B_Button19_IDJAVA = findViewById(R.id.B_Button19_IDXML);
+        B_Button20_IDJAVA = findViewById(R.id.B_Button20_IDXML);
+        B_Button21_IDJAVA = findViewById(R.id.B_Button21_IDXML);
+        B_Button22_IDJAVA = findViewById(R.id.B_Button22_IDXML);
+        B_Button23_IDJAVA = findViewById(R.id.B_Button23_IDXML);
+        B_Button24_IDJAVA = findViewById(R.id.B_Button24_IDXML);
+        B_Button25_IDJAVA = findViewById(R.id.B_Button25_IDXML);
+    }
+}
